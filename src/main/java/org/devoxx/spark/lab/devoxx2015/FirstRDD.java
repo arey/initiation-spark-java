ee81bbe4b94bb19749dd5b5536a7541f19bd6b80
Antoine Rey <antoine.rey@free.fr>
2015-12-13 16:47:27 +0100

    Fix #1 Windows support

--- src/main/java/org/devoxx/spark/lab/devoxx2015/FirstRDD.java
+++ src/main/java/org/devoxx/spark/lab/devoxx2015/FirstRDD.java
@@ -4,17 +4,29 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 
+import java.io.File;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class FirstRDD {
 
     public static void main(String... args) {
-        SparkConf conf = new SparkConf().setAppName("Workshop").setMaster("local[*]");
+        FirstRDD runner = new FirstRDD();
+        SparkConf conf = new SparkConf().setAppName("Sparky CDN").setMaster("local[*]");
         JavaSparkContext sc = new JavaSparkContext(conf);
 
-        Path path = Paths.get(FirstRDD.class.getResource("/ratings.txt").getPath());
+        Path path = runner.getFilePath(runner.getFile());
         JavaRDD<String> lines = sc.textFile(path.toString());
         System.out.println("Lines count: " + lines.count());
+    }
+
+    private File getFile() {
+        ClassLoader classLoader = getClass().getClassLoader();
+        return new File(classLoader.getResource("ratings.txt").getFile());
+    }
+
+    private Path getFilePath(File file) {
+        Path path = Paths.get(file.toURI());
+        return path;
     }
 }
